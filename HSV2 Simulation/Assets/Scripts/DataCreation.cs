5e97fde6d4fa461cf9b3d03a8e9c157520cbaa55
WillPyetheCSguy <willpye00@gmail.com>
2020-03-20 17:05:28 -0700

    Upload from Gitlab to Github

---
+++ HSV2 Simulation/Assets/Scripts/DataCreation.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCreation : MonoBehaviour {
+
+    public float femaleToMaleInfectionRate = 2.5f;
+    public float maleToFemaleInfectionRate = 2.82f;
+    public int newFemaleStudents = 5500;
+    public int newMaleStudents = 4500;
+    //public float propFemaleVacinatedPre = .02f;
+    //public float efficacyOfVaccine = .07f;
+    // .010556 seropostive females .0054438 seropositive males
+    public float propSeroNegativeFemalePre = .98944f;
+    public float propSeroNegativeMalePre = .99455f;
+    //public float propFemalsVacinated = .03f;
+    public float propFemalesLeaving = 0.2f;
+    public float propMalesLeaving = 0.2f;
+    public int startingMalePop = 15000;
+    public int startingFemalePop = 20000;
+    public float propStartingMaleInfected = .0f;
+    public float propStartingFemaleInfected = .0f;
+    public int numStartingMaleInfected = 108;
+    public int numStartingFemaleInfected = 254;
+    public float avgInfectedMaleToFemale = .4f;
+    public float avgInfectedFemaleToMale = .3f;
+
+    private int infectedFemales;
+    private int infectedMales;
+    private int susceptibleFemales;
+    private int susceptibleMales;
+    //private int vacinatedFemales
+
+    private float lastUpdate;
+    private int step;
+
+    // Use this for initialization
+    void Start () {
+        infectedFemales = (int)(propStartingFemaleInfected * startingFemalePop);
+        infectedMales = (int)(propStartingMaleInfected * startingMalePop);
+        //infectedMales = numStartingMaleInfected;
+        //infectedFemales = numStartingFemaleInfected;
+        susceptibleFemales = startingFemalePop - infectedFemales;
+        susceptibleMales = startingMalePop - infectedMales;
+        lastUpdate = Time.time;
+        step = 0;
+        Debug.Log(step);
+        Debug.Log("UF");
+        Debug.Log(susceptibleFemales);
+        Debug.Log("UM");
+        Debug.Log(susceptibleMales);
+        Debug.Log("IF");
+        Debug.Log(infectedFemales);
+        Debug.Log("IM");
+        Debug.Log(infectedMales);
+        //
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > lastUpdate + 2)
+        {
+            CalcTimestep();
+            step++;
+            Debug.Log(step);
+            Debug.Log("UF");
+            Debug.Log(susceptibleFemales);
+            Debug.Log("UM");
+            Debug.Log(susceptibleMales);
+            Debug.Log("IF");
+            Debug.Log(infectedFemales);
+            Debug.Log("IM");
+            Debug.Log(infectedMales);
+            Debug.Log("%IF");
+            Debug.Log((float)infectedFemales / ((float)susceptibleFemales + (float)infectedFemales));
+            Debug.Log("%IM");
+            Debug.Log((float)infectedMales / ((float)susceptibleMales + (float)infectedMales));
+            lastUpdate = Time.time;
+        }
+	}
+
+    void CalcTimestep()
+    {
+        //int tempSusceptibleFemales = (int)(propSeroNegativeFemalePre * newFemaleStudents + susceptibleFemales * (1 - ((maleToFemaleInfectionRate * infectedMales) / (susceptibleFemales + infectedFemales))) - propFemalesLeaving * susceptibleFemales);
+        //int tempSusceptibleMales = (int)(propSeroNegativeMalePre * newMaleStudents + susceptibleMales * (1 - ((femaleToMaleInfectionRate * infectedFemales) / (susceptibleMales + infectedMales))) - propMalesLeaving * susceptibleMales);
+        //int tempInfectedMales = (int)(newMaleStudents * (1 - propSeroNegativeMalePre) + infectedMales * (1 - propMalesLeaving) + femaleToMaleInfectionRate * infectedFemales * (susceptibleMales / (infectedMales + susceptibleMales)));
+        //int tempInfectedFemales = (int)(newFemaleStudents * (1 - propSeroNegativeFemalePre) + infectedFemales * (1 - propFemalesLeaving) + maleToFemaleInfectionRate * infectedMales * (susceptibleFemales / (infectedFemales + susceptibleFemales)));
+        int tempSusceptibleFemales = (int)(propSeroNegativeFemalePre * newFemaleStudents + susceptibleFemales  - ((avgInfectedMaleToFemale * infectedMales)) - propFemalesLeaving * susceptibleFemales);
+        int tempSusceptibleMales = (int)(propSeroNegativeMalePre * newMaleStudents + susceptibleMales -  ((avgInfectedFemaleToMale * infectedFemales)) - propMalesLeaving * susceptibleMales);
+        int tempInfectedMales = (int)(newMaleStudents * (1 - propSeroNegativeMalePre)  + infectedMales * (1 - propMalesLeaving) + avgInfectedFemaleToMale * infectedFemales);
+        int tempInfectedFemales = (int)(newFemaleStudents * (1 - propSeroNegativeFemalePre) + infectedFemales * (1 - propFemalesLeaving) + avgInfectedMaleToFemale * infectedMales);
+        infectedFemales = tempInfectedFemales;
+        susceptibleMales = tempSusceptibleMales;
+        susceptibleFemales = tempSusceptibleFemales;
+
+    }
+}
---
+++ HSV2 Simulation/Assets/Scripts/DifferentialCreation.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifferentialCreation : MonoBehaviour {
+
+
+    //Population Variables
+    private float primaryInfected;
+    private float maturedInfected;
+    private float susceptible;
+    private float vacinated;
+    private float N = 30000f;
+
+    //Starting values
+    public int startPrimaryInfected = 27;
+    public int startMaturedInfected = 900;
+    public int startSusceptible = 29073;
+    public int startVacinated = 0;
+
+    //Time variables
+    public int tN = 1825;
+    public int tI = 18;
+
+    //Infection variables
+    public float pI = .9f;
+    public float pM = 0.122f;
+
+    //Vaccination variables
+    public float u = .0f;
+
+    //Contact variables
+    public float c = 2f/365f;
+
+    //Time variables
+    private int stepCount;
+    private float lastUpdate;
+    public float timeBetweenSteps = 1.5f;
+
+    //Integer Values For Displaying
+    private int intPI;
+    private int intMI;
+    private int intS;
+    private int intV;
+    private int intN;
+
+    //Display Objects
+    public Text stats;
+    public Text init;
+    public Text param;
+
+	// Use this for initialization
+	void Start () {
+        primaryInfected = startPrimaryInfected;
+        maturedInfected = startMaturedInfected;
+        susceptible = startSusceptible;
+        vacinated = startVacinated;
+        stepCount = 0;
+        lastUpdate = Time.time;
+        init.text = "Initial Values:\nSusceptible: " + startSusceptible + "\nPrimary Infected: " + startPrimaryInfected + "\nMatured Infected: " + startMaturedInfected + "\nVacinated: " + startVacinated;
+        param.text = "Unique Partners per Day: " + c + "\nPercent of Susceptible vacinated per Day: " + u * 100 + "%\nPrimary Infection Chance: " + pI + "\nMatured Infection Chance: "+ pM + "\nTime in Primary: " + tI + " days\nTime in Population: " + tN + " days";
+        UpdateText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > lastUpdate + timeBetweenSteps)
+        {
+            CalcTimeStep();
+            stepCount++;
+           // Debug.Log(stepCount);
+           // Debug.Log("I");
+            //Debug.Log(primaryInfected);
+            //Debug.Log("M");
+            //Debug.Log(maturedInfected);
+            //Debug.Log("S");
+            //Debug.Log(susceptible);
+            //Debug.Log("V");
+            //Debug.Log(vacinated);
+            lastUpdate = Time.time;
+            intPI = (int)primaryInfected;
+            intMI = (int)maturedInfected;
+            intS = (int)susceptible;
+            intV = (int)vacinated;
+            intN = intV + intS + intMI + intPI;
+            UpdateText();
+        }
+    }
+
+    void UpdateText()
+    {
+        stats.text = "Day: " + stepCount + "\nSusceptible: " + intS + "\nPrimary Infected: " + intPI + "\nMatured Infected: " + intMI + "\nVacinated: " + intV;
+        
+    }
+
+
+    void CalcTimeStep()
+    {
+
+        float tempSus = susceptible + (.992f * N / tN) - (susceptible * ((primaryInfected * pI + maturedInfected * pM) * (c / N) + (1f / tN) + u));
+        //float tempSus = susceptible + (N / tN) - (susceptible * ((primaryInfected * pI + maturedInfected * pM) * c / N + (1f / tN) + u));
+        float tempInf = primaryInfected + (susceptible * (primaryInfected * pI + maturedInfected * pM) * c / N) - primaryInfected * (1f / tI + 1f / tN);
+        float tempMat = maturedInfected + (.008f * N / tN) + primaryInfected * (1f / tI) - maturedInfected * (1f / tN);
+        //float tempMat = maturedInfected + primaryInfected * (1f / tI) - maturedInfected * (1f / tN);
+        float tempVac = vacinated + (susceptible * u) - (vacinated / tN);
+        susceptible = tempSus;
+        primaryInfected = tempInf;
+        maturedInfected = tempMat;
+        vacinated = tempVac;
+    }
+}
